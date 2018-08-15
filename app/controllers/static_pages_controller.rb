5da1ebd9a618b2b2c6a90f8555b013aed8a3b4a1
Sean <sean.n.cordes@gmail.com>
2018-08-15 18:19:48 -0400

    basic layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,16 +2,14 @@ class StaticPagesController < ApplicationController
   require 'open-uri'
 
   def home
-    response = JSON.parse(open(
-                            "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-                          ).read).first(30)
-    @news = []
-    response.each do |id|
-      news = JSON.parse(open(
-                          "https://hacker-news.firebaseio.com/v0/item/#{id}.json?print=pretty"
-                        ).read, symbolize_names: true)
-      @news << news
-    end
+    url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+    @response = JSON.parse(open(url).read)
     @n = 0
+    @shift = 0
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
