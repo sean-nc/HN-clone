aa6bc38d1919aadd1736a578190988d9c9fb3ab3
Sean <sean.n.cordes@gmail.com>
2018-08-15 23:10:33 -0400

    infinite scroll with jquery

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,12 @@
 class StaticPagesController < ApplicationController
   require 'open-uri'
+  require 'will_paginate/array'
 
   def home
     url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
     @response = JSON.parse(open(url).read)
+    @response = @response.paginate(page: params[:page], per_page: 15)
     @n = 0
-    @shift = 0
 
     respond_to do |format|
       format.html
